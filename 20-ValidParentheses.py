0ebb997c7f6c27d751d8fa226245eb3e36d22b7c
vgthomson <thomsonvg@outlook.com>
2025-03-18 23:33:46 +0530

    Parenthesis Problem Solved

--- 20-ValidParentheses.py
+++ 20-ValidParentheses.py
@@ -1,33 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stacks = []
+        brackets = {"(":")","[":"]","{":"}"}
+        stack = []
         for i in s:
-            if i == "(" or i =="{" or i =="[":
-                stacks.append(i)
-                print(i)
-            elif i==")":
-                if len(stacks)== 0:
-                    stacks.append(i)
-                elif stacks[-1] == "(":
-                    stacks.remove("(")
-            elif i=="}":
-                if len(stacks)== 0:
-                    stacks.append(i)
-                elif stacks[-1] == "{":
-                    stacks.remove("{")
-            elif i=="]":
-                print(i)
-                if len(stacks)== 0:
-                    stacks.append(i)
-                elif stacks[-1] == "[":
-                    stacks.remove("[")
-                    
-        if len(stacks) == 0:
-            return True
-        else:
-            return False
-
+            if i in '({[':
+                stack.append(i)
+            elif len(stack)==0 or i != brackets[stack.pop()]:
+                return False
+        return len(stack)
 
 solution = Solution()
-result = solution.isValid("(])")
+result = solution.isValid("]")
 print(result)
